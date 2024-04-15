78da836a0c1f9560000b700144fb98b6ab3f7f77
Vivek Malipatel <77662002+VivekMalipatel@users.noreply.github.com>
2024-04-15 10:42:35 -0500

    commit

--- Dataset/translate.py
+++ Dataset/translate.py
@@ -7,21 +7,15 @@ translator = GoogleTranslator(source='en', target='es')
 
 def translate_text(text):
     try:
-        # Translate the text to Spanish using DeepL (as an example)
         translated_text = translator.translate(text)
-        print("Original text:", text)
-        print("Translated text:", translated_text)
         return str(translated_text)
     except Exception as e:
-        # In case of any error, return the original text
         print("Error during translation:", e)
         return "NAAN" + text
 
 
 data_path = 'Dataset/Testing_dataset/pre_processed_text_test_partition.csv'
 data = pd.read_csv(data_path)
-
-data = data.sample(5)
 
 print("Translating text to Spanish...")
 data['text'] = data['text'].progress_apply(translate_text)
